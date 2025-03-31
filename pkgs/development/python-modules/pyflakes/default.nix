e950c21264d5e43daa8d7ce7019abaf24e073e49
Robert Schütz <nix@dotlambda.de>
2025-03-30 22:00:18 -0700

    python313Packages.pyflakes: 3.2.0 -> 3.3.1
    Diff: https://github.com/PyCQA/pyflakes/compare/refs/tags/3.2.0...3.3.1
    
    Changelog: https://github.com/PyCQA/pyflakes/blob/3.3.1/NEWS.rst

--- pkgs/development/python-modules/pyflakes/default.nix
+++ pkgs/development/python-modules/pyflakes/default.nix
@@ -3,7 +3,6 @@
   buildPythonPackage,
   isPyPy,
   pythonAtLeast,
-  pythonOlder,
   fetchFromGitHub,
   setuptools,
   pytestCheckHook,
@@ -11,40 +10,32 @@
 
 buildPythonPackage rec {
   pname = "pyflakes";
-  version = "3.2.0";
-
-  disabled = pythonOlder "3.8";
-
+  version = "3.3.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "PyCQA";
     repo = "pyflakes";
-    rev = version;
-    hash = "sha256-ouCkkm9OrYob00uLTilqgWsTWfHhzaiZp7sa2C5liqk=";
+    tag = version;
+    hash = "sha256-JgHftAoH9wnqjkWp6AbYDHKbBRMps+M/idPalc/S5Uo=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
-  disabledTests =
-    lib.optionals (pythonAtLeast "3.13") [
-      # https://github.com/PyCQA/pyflakes/issues/812
-      "test_errors_syntax"
-    ]
-    ++ lib.optionals isPyPy [
-      # https://github.com/PyCQA/pyflakes/issues/779
-      "test_eofSyntaxError"
-      "test_misencodedFileUTF8"
-      "test_multilineSyntaxError"
-    ];
+  disabledTests = lib.optionals isPyPy [
+    # https://github.com/PyCQA/pyflakes/issues/779
+    "test_eofSyntaxError"
+    "test_misencodedFileUTF8"
+    "test_multilineSyntaxError"
+  ];
 
   pythonImportsCheck = [ "pyflakes" ];
 
   meta = with lib; {
     homepage = "https://github.com/PyCQA/pyflakes";
-    changelog = "https://github.com/PyCQA/pyflakes/blob/${src.rev}/NEWS.rst";
+    changelog = "https://github.com/PyCQA/pyflakes/blob/${src.tag}/NEWS.rst";
     description = "Simple program which checks Python source files for errors";
     mainProgram = "pyflakes";
     license = licenses.mit;
